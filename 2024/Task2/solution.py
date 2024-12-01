76f24b20860db33d79892f3bcc507cc37ed1ab82
Neyromant <Neyromant777@gmail.com>
2024-12-01 08:42:29 +0300

    ffdsf

---
+++ 2024/Task2/solution.py
@@ -0,0 +1,12 @@
+#https://adventofcode.com/2024/day/1
+list1, list2 = [], []
+for line in open('input.txt'):
+    a, b = line.split()
+    list1.append(int(a))
+    list2.append(int(b))
+list1.sort(reverse=True)
+list2.sort(reverse=True)
+summ = sum([abs(i-list2[idx]) for idx, i in enumerate(list1)])
+print(f"Решение части 1 {summ}")
+power = sum([i*list2.count(i) for i in list1])
+print(f"Решение части 2 {power}")
