a62fc82ea4c8d37950445f15c7faa12185c2c9f0
Neyromant <Neyromant777@gmail.com>
2024-12-01 08:18:26 +0300

    ffdsf

--- 2024/Task1/solution.py
+++ 2024/Task1/solution.py
@@ -1,14 +1,17 @@
-#https://adventofcode.com/2023/day/1
-list_str = [i.strip("\n ") for i in open("input.txt")]
-def get_fist_digit(str_in, reverse=False, part =1):
-    dict_convert = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9,}
-    for idx, i in enumerate(str_in):
-        if i.isdigit():
-            return i
-        if part ==2:
-            for key, val in dict_convert.items():
-                key_ = key if not reverse else key[::-1]
-                if key_ in str_in[:idx+1]:
-                    return str(dict_convert[key])
-print(f"Решение задания 1: {sum([(int(get_fist_digit(i)+get_fist_digit(i[::-1], reverse=True))) for i in list_str])}")
-print(f"Решение задания 2: {sum([(int(get_fist_digit(i, part=2)+get_fist_digit(i[::-1], reverse=True, part=2))) for i in list_str])}")
+#https://adventofcode.com/2024/day/1
+list1, list2 = [], []
+f = open('input.txt')
+for line in f:
+    a, b = line.split()
+    list1.append(int(a))
+    list2.append(int(b))
+list1.sort(reverse=True)
+list2.sort(reverse=True)
+summ = 0
+for idx, i in enumerate(list1):
+    summ += abs(int(i)-list2[idx])
+print(summ)
+power = 0
+for i in list1:
+    power += i*list2.count(i)
+print(power)
