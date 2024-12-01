48e205fe2263b6103554bc244326850571d43fe8
Neyromant <Neyromant777@gmail.com>
2024-12-01 10:01:52 +0300

    ffdsf

--- 2024/Task1/solution.py
+++ 2024/Task1/solution.py
@@ -4,7 +4,6 @@ for line in open('input.txt'):
     a, b = line.split()
     list1.append(int(a))
     list2.append(int(b))
-list1.sort(reverse=True)
-list2.sort(reverse=True)
+[i.sort(reverse=True) for i in [list1, list2]]
 print(f"Решение части 1 {sum([abs(i-list2[idx]) for idx, i in enumerate(list1)])}")
 print(f"Решение части 2 {sum([i*list2.count(i) for i in list1])}")
