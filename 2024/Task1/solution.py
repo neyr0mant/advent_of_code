94e0ebb845fab562a58da25edfc1234efaffa953
Neyromant <Neyromant777@gmail.com>
2024-12-01 10:25:48 +0300

    ffdsf

--- 2024/Task1/solution.py
+++ 2024/Task1/solution.py
@@ -1,9 +1,6 @@
 #https://adventofcode.com/2024/day/1
-list1, list2 = [], []
-for line in open('input.txt'):
-    a, b = line.split()
-    list1.append(int(a))
-    list2.append(int(b))
+list_str = [i.strip("\n ").split() for i in open("input.txt")]
+list1, list2 = [int(i[0]) for i in list_str], [int(i[1]) for i in list_str]
 [i.sort(reverse=True) for i in [list1, list2]]
 print(f"Решение части 1 {sum([abs(i-list2[idx]) for idx, i in enumerate(list1)])}")
 print(f"Решение части 2 {sum([i*list2.count(i) for i in list1])}")
