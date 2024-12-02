c27c97814c55b76011f47e6bde35e336fce2eae1
Neyromant <Neyromant777@gmail.com>
2024-12-02 14:20:39 +0300

    ffdsf

--- 2024/Task2/solution.py
+++ 2024/Task2/solution.py
@@ -1,12 +1,18 @@
-#https://adventofcode.com/2024/day/1
-list1, list2 = [], []
-for line in open('input.txt'):
-    a, b = line.split()
-    list1.append(int(a))
-    list2.append(int(b))
-list1.sort(reverse=True)
-list2.sort(reverse=True)
-summ = sum([abs(i-list2[idx]) for idx, i in enumerate(list1)])
-print(f"Решение части 1 {summ}")
-power = sum([i*list2.count(i) for i in list1])
-print(f"Решение части 2 {power}")
+#https://adventofcode.com/2024/day/2
+list_data = [[int(j) for j in i.strip("\n ").split()] for i in open("input.txt")]
+def get_good_report(report, part=1):
+    list_diff = [(report[idx + 1] - i, idx) for idx, i in enumerate(report) if idx + 1 != len(report)]
+    list_more = [i for i in list_diff if 3 >= i[0] > 0]
+    list_less = [i for i in list_diff if -3 <= i[0] < 0]
+    if len(list_more) == len(list_diff) or len(list_less) == len(list_diff):
+        return True
+print(f"Решение части 1: {len([i for i in list_data if get_good_report(i)])}")
+
+
+
+
+
+
+
+
+
