3a3bc919607c6c0baa77a87bf725008092995d75
Neyromant <Neyromant777@gmail.com>
2024-12-03 14:07:50 +0300

    ffdsf

--- 2024/Task3/solution.py
+++ 2024/Task3/solution.py
@@ -1,22 +1,35 @@
-#https://adventofcode.com/2024/day/2
-list_data = [[int(j) for j in i.strip("\n ").split()] for i in open("input.txt")]
-from copy import deepcopy
-def get_good_report(report, part=1):
-    list_diff = [(report[idx + 1] - i) for idx, i in enumerate(report) if idx + 1 != len(report)]
-    list_more = [i for i in list_diff if 3 >= i > 0]
-    list_less = [i for i in list_diff if -3 <= i < 0]
-    if any([len(list_more) == len(list_diff), len(list_less) == len(list_diff)]):
-        return True
-    if part == 2:
-        for i in range(len(report)):
-            copy_report = deepcopy(report)
-            copy_report.pop(i)
-            if get_good_report(copy_report, part=1):
-                return True
-        return False
+#https://adventofcode.com/2024/day/3
+list_data = [i for i in open("input.txt")]
+data = "".join(list_data)
+test_data = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
-print(f"Решение части 1: {len([i for i in list_data if get_good_report(i)])}")
-print(f"Решение части 2: {len([i for i in list_data if get_good_report(i, part=2)])}")
+def get_multiplication(str_in, part=1):
+    list_data = str_in.split("mul")
+    summ = 0
+    dont = False
+    for i in list_data:
+        list_i =[i.split(")") for i in i.split("(")]
+        if len(list_i) < 2:
+            continue
+        if part ==2:
+            if dont:
+                if "do()" in i:
+                    dont = False
+                continue
+        if list_i[0] == [""]:
+            a_b_list = [int(i) for i in list_i[1][0].split(",") if i.isdigit()]
+            if len(a_b_list) == 2:
+                summ += a_b_list[0]*a_b_list[1]
+        if part == 2:
+            if "don't()" in i:
+                dont = True
+            if dont:
+                if "do()" in i:
+                    dont = False
+    return summ
+
+print(get_multiplication(data, part=2))
+
 
 
 
