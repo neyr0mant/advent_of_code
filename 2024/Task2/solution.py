7ee7b3e14fa21aa609c3a08b2a9cbddd79471cf0
Neyromant <Neyromant777@gmail.com>
2024-12-03 03:29:42 +0300

    ffdsf

--- 2024/Task2/solution.py
+++ 2024/Task2/solution.py
@@ -1,23 +1,33 @@
 #https://adventofcode.com/2024/day/2
 list_data = [[int(j) for j in i.strip("\n ").split()] for i in open("input.txt")]
-from copy import deepcopy
+
+
 def get_good_report(report, part=1):
-    list_diff = [(report[idx + 1] - i) for idx, i in enumerate(report) if idx + 1 != len(report)]
-    list_more = [i for i in list_diff if 3 >= i > 0]
-    list_less = [i for i in list_diff if -3 <= i < 0]
-    if any([len(list_more) == len(list_diff), len(list_less) == len(list_diff)]):
-        return True
-    if part == 2:
-        for i in range(len(report)):
-            copy_report = deepcopy(report)
-            copy_report.pop(i)
-            if get_good_report(copy_report, part=1):
-                return True
-        return False
+    last_good_diff, len_report = 0, len(report)
+    for idx in range(len_report-1):
+        cur_num, next_num = report[idx], report[idx + 1]
+        diff = next_num - cur_num
+        if any([diff > 3, diff < -3, diff == 0, diff > 0 > last_good_diff, diff < 0 < last_good_diff]):
+            if part == 1:
+                return False
+            else:
+                if idx == len_report-1:
+                    return True
+                elif idx == 0:
+                    return get_good_report(report[1:], part=1)
+                else:
+                    new_report_1 = report[:idx-1] + report[idx:]
+                    new_report_2 = report[:idx] + report[idx+1:]
+                    new_report_3 = report[:idx+1] + report[idx+2:]
+                    return any([get_good_report(new_report_1, part=1),
+                                get_good_report(new_report_2, part=1),
+                                get_good_report(new_report_3, part=1)])
+        last_good_diff = diff
+    return True
+
 
 print(f"Решение части 1: {len([i for i in list_data if get_good_report(i)])}")
 print(f"Решение части 2: {len([i for i in list_data if get_good_report(i, part=2)])}")
-
 
 
 
