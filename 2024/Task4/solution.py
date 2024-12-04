16597b84341f4b586f3eb7711847c292442bb9c5
Neyromant <Neyromant777@gmail.com>
2024-12-04 23:33:50 +0300

    ffdsf

--- 2024/Task4/solution.py
+++ 2024/Task4/solution.py
@@ -1,24 +1,11 @@
 #https://adventofcode.com/2024/day/4
-list_data = [i.strip("\n") for i in open("input.txt")]
+list_data = [i.strip("\n") for i in open("input2.txt")]
 from functions import execution_time
-DATA_V1 = [
-    [[1, 0], [2, 0], [3, 0]],
-    [[1, 1], [2, 2], [3, 3]],
-    [[0, 1], [0, 2], [0, 3]],
-    [[-1, 1], [-2, 2], [-3, 3]],
-    [[-1, 0], [-2, 0], [-3, 0]],
-    [[-1, -1], [-2, -2], [-3, -3]],
-    [[0, -1], [0, -2], [0, -3]],
-    [[1, -1], [2, -2], [3, -3]],
-]
-DATA_V2 = [
-    [[-1, -1], [1, -1], [1, 1], [-1, 1]],
-    [[1, -1], [1, 1], [-1, -1], [-1, 1]],
-    [[1, 1], [-1, 1], [-1, -1], [1, -1]],
-    [[-1, 1], [-1, -1], [1, 1], [1, -1]],
-]
-
-
+DATA_V1 = [[[1, 0], [2, 0], [3, 0]],[[1, 1], [2, 2], [3, 3]],[[0, 1], [0, 2], [0, 3]],[[-1, 1], [-2, 2], [-3, 3]],
+           [[-1, 0], [-2, 0], [-3, 0]],[[-1, -1], [-2, -2], [-3, -3]],[[0, -1], [0, -2], [0, -3]],
+           [[1, -1], [2, -2], [3, -3]],]
+DATA_V2 = [[[-1, -1], [1, -1], [1, 1], [-1, 1]],[[1, -1], [1, 1], [-1, -1], [-1, 1]],
+           [[1, 1], [-1, 1], [-1, -1], [1, -1]],[[-1, 1], [-1, -1], [1, 1], [1, -1]]]
 def search(x, y, list_word, matrix, word_find):
     count_ = 0
     for data in matrix:
@@ -37,7 +24,6 @@ def search(x, y, list_word, matrix, word_find):
             if len_ == len(word_find):
                 count_ += 1
     return count_
-
 @execution_time
 def get_count_word(list_word, part=1):
     matrix, word_find, letter_find = (DATA_V1,'MAS', "X") if part == 1 else (DATA_V2, 'MMSS', "A")
