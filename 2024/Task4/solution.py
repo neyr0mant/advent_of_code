f5af49debe3b6d6282debf7dec707e42f4db174b
Neyromant <Neyromant777@gmail.com>
2024-12-04 23:40:29 +0300

    ffdsf

--- 2024/Task4/solution.py
+++ 2024/Task4/solution.py
@@ -8,11 +8,11 @@ DATA_V2 = [[[-1, -1], [1, -1], [1, 1], [-1, 1]],[[1, -1], [1, 1], [-1, -1], [-1,
            [[1, 1], [-1, 1], [-1, -1], [1, -1]],[[-1, 1], [-1, -1], [1, 1], [1, -1]]]
 
 def search(x, y, list_word, matrix, word_find):
-    count_ = 0
+    count = 0
     for data in matrix:
-        len_ = 0
-        for index, delta_ in enumerate(data):
-            x_delta, y_delta = delta_
+        len_search = 0
+        for index, delta in enumerate(data):
+            x_delta, y_delta = delta
             x_, y_ = x + x_delta, y + y_delta
             if x_ < 0 or y_ < 0:
                 break
@@ -21,19 +21,19 @@ def search(x, y, list_word, matrix, word_find):
                     break
             except IndexError:
                 break
-            len_ += 1
-            if len_ == len(word_find):
-                count_ += 1
-    return count_
+            len_search += 1
+            if len_search == len(word_find):
+                count += 1
+    return count
 
 def get_count_word(list_word, part=1):
     matrix, word_find, letter_find = (DATA_V1,'MAS', "X") if part == 1 else (DATA_V2, 'MMSS', "A")
-    result = 0
+    count_all = 0
     for y, line in enumerate(list_word):
         for x, letter in enumerate(line):
             if list_word[y][x] == letter_find:
-                result += search(x, y, list_word, matrix, word_find)
-    return result
+                count_all += search(x, y, list_word, matrix, word_find)
+    return count_all
 
 
 print(f"Решение части 1: {get_count_word(list_data, part=1)}")
