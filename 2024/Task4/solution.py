fa9712086f2790dd786554621f45d2daeb3f3049
Neyromant <Neyromant777@gmail.com>
2024-12-04 22:45:24 +0300

    ffdsf

--- 2024/Task4/solution.py
+++ 2024/Task4/solution.py
@@ -1,5 +1,6 @@
 #https://adventofcode.com/2024/day/4
 list_data = [i.strip("\n") for i in open("input.txt")]
+from functions import execution_time
 DATA_V1 = [
     [[1, 0], [2, 0], [3, 0]],
     [[1, 1], [2, 2], [3, 3]],
@@ -37,8 +38,8 @@ def search(x, y, list_word, matrix, word_find):
                 count_ += 1
     return count_
 
-
-def get_count_word(list_word, part =1):
+@execution_time
+def get_count_word(list_word, part=1):
     matrix, word_find, letter_find = (DATA_V1,'MAS', "X") if part == 1 else (DATA_V2, 'MMSS', "A")
     result = 0
     for y, line in enumerate(list_word):
@@ -46,6 +47,7 @@ def get_count_word(list_word, part =1):
             if list_word[y][x] == letter_find:
                 result += search(x, y, list_word, matrix, word_find)
     return result
+
 
 print(f"Решение части 1: {get_count_word(list_data, part=1)}")
 print(f"Решение части 2: {get_count_word(list_data, part=2)}")
