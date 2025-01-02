8864d151dc1c272854529406c96dc8653091af9f
Neyromant <Neyromant777@gmail.com>
2025-01-03 00:27:11 +0300

    ffdsf

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import time
 from copy import deepcopy
 
+
 def execution_time(func):
     def wrapped(*args, **kwargs):
         time_start = time.time()
@@ -9,6 +10,7 @@ def execution_time(func):
         execution_time = time_finish - time_start
         print("Script execution time: %s sec" % execution_time)
         return res
+
     return wrapped
 
 
@@ -22,6 +24,12 @@ class Matrix():
     @staticmethod
     def get_rank_matrix(matrix: list):
         return len(matrix[0]), len(matrix)
+
+    def update_matrix(self, dict_elements: dict, revert_x_y = False):
+        for elem, list_x_y in dict_elements.items():
+            for x_y in list_x_y:
+                x, y = x_y if not revert_x_y else x_y[::-1]
+                self.list_data[y][x] = elem
 
     def __getitem__(self, x_y: tuple):
         return self.list_data[x_y[1]][x_y[0]]
@@ -41,10 +49,12 @@ class Matrix():
             new_matrix = self.get_matrix_with_change_elements(dict_elements)
             x_max, y_max = self.get_rank_matrix(new_matrix)
             list_matrix = new_matrix
-        print(f"""######## Матрица размером {x_max}x{y_max} ########""")
-        for i in list_matrix:
-            list_print = " ".join([j for j in i])
-            print(list_print)
+        print(f"""Матрица размером {x_max}x{y_max}""")
+        print(f"""Расположение осей""")
+        print(f"""-- -- -- Y\n¦\n¦\n¦\nX""")
+        res_matrix = "\n".join([" ".join([str(j) for j in i]) for i in list_matrix])
+        print(res_matrix)
+        return res_matrix
 
     def get_matrix_with_change_elements(self, dict_elements: dict):
         new_matrix = deepcopy(self.list_data)
@@ -54,9 +64,3 @@ class Matrix():
                 x, y = coordinate
                 new_matrix[y][x] = elem
         return new_matrix
-
-
-
-
-
-
