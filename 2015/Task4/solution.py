a248abd6e7d410428e988db4dd9fae59da020886
Neyromant <Neyromant777@gmail.com>
2025-01-05 13:00:00 +0300

    ffdsf

--- 2015/Task4/solution.py
+++ 2015/Task4/solution.py
@@ -1,21 +1,16 @@
 import hashlib
-def get_min_count(data_str, part =1):
+
+
+def get_min_count(data_str, count_zero):
     i = 1
-    count_zero = 5*"0" if part == 1 else 6*"0"
     while True:
         md5_hash = hashlib.md5()
         md5_hash.update((data_str + str(i)).encode('utf-8'))
         hex_digest = md5_hash.hexdigest()
-        if hex_digest.startswith(count_zero):
+        if hex_digest.startswith(count_zero * "0"):
             return i
         i += 1
-print(f"Решение части 1: {get_min_count('iwrupvqb')}")
-print(f"Решение части 2: {get_min_count('iwrupvqb', part=2)}")
 
 
-
-
-
-
-
-
+print(f"Решение части 1: {get_min_count('iwrupvqb', 5)}")
+print(f"Решение части 2: {get_min_count('iwrupvqb', 6)}")
