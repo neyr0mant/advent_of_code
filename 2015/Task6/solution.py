08da75a67d8c6542bc535c2cee79f960ed6c66f6
Neyromant <Neyromant777@gmail.com>
2025-01-05 21:51:59 +0300

    ffdsf

--- 2015/Task6/solution.py
+++ 2015/Task6/solution.py
@@ -10,73 +10,46 @@ def generate_test_data(rank, count_rule = 5):
             f" through {random.choice(list_index)},{random.choice(list_index)}" for _ in range(count_rule)]
 
 @execution_time
-def get_count_lamp(rank, list_rule, print_all=False, out_file= False, part=1):
+def get_solve(rank, list_rule, part):
     grid = Matrix([[0 for _ in range(rank)] for _ in range(rank)])
-    msg = 'Зажжены' if part == 1 else "Яркость"
-    if print_all:
-        print("Начальная сетка")
-        print(f"{msg}: {sum([sum([x for x in x_list]) for x_list in grid.list_data])} лампочек")
-        grid.print_matrix()
 
-    def get_rule_for_str(rule_str_):
-        for type_rule in ["toggle", "turn off", "turn on"]:
-            if type_rule in rule_str_:
-                list_coordinate = [[int(j) for j in i.split(",")] for i in
-                                   rule_str_.split(type_rule)[1].split("through")]
-                return {type_rule: list_coordinate}
+    def update_grid(list_coordinate, key, part_):
+        if part_ == 1:
+            if key == "turn on":
+                grid.update_matrix_list_data({1: list_coordinate})
+            elif key == "turn off":
+                grid.update_matrix_list_data({0: list_coordinate})
+            elif key == "toggle":
+                for x, y in list_coordinate:
+                    elem = 1 if grid[x, y] == 0 else 0
+                    grid.update_matrix_element(elem, [x, y])
+        else:
+            for x, y in list_coordinate:
+                cur_bright = grid[x, y]
+                if key == "turn on":
+                    cur_bright += 1
+                elif key == "turn off":
+                    cur_bright -= 1 if cur_bright >= 1 else 0
+                elif key == "toggle":
+                    cur_bright += 2
+                grid.update_matrix_element(cur_bright, [x, y])
 
-    def get_list_coordinate_for_change(list_coordinate_):
-        list_out = []
-        list_x, list_y = [i[1] for i in list_coordinate_], [i[0] for i in list_coordinate_]
-        x_min, y_min, x_max, y_max = min(list_x), min(list_y), max(list_x), max(list_y)
-        for y in range(y_min, y_max+1):
-            for x in range(x_min, x_max+1):
-                if (0 <= x <= rank-1) and (0 <= y <= rank-1):
-                    list_out.append([x, y])
-        return list_out
-
-    for idx, rule_str in enumerate(list_rule):
-        rule = get_rule_for_str(rule_str)
-        list_coordinate_for_change = get_list_coordinate_for_change(list(rule.values())[0])
-        rule_key = list(rule.keys())[0]
-        if rule_key == "turn on":
-            if part == 1:
-                grid.update_matrix({1: list_coordinate_for_change}, revert_x_y=True)
+    for rule_str in list_rule:
+        rule_key = [i for i in ["toggle", "turn off", "turn on"] if i in rule_str][0]
+        start_coordinate = [[int(j) for j in i.split(",")] for i in rule_str.split(rule_key)[1].split("through")]
+        a, b = [start_coordinate[0][0], start_coordinate[1][0]], [start_coordinate[0][1], start_coordinate[1][1]]
+        x_min, x_max, y_min, y_max = min(a), max(a), min(b), max(b)
+        coordinate_for_change = []
+        if x_max != x_min:
+            if y_max != y_min:
+                [[coordinate_for_change.append([x, y]) for x in range(x_min, x_max+1)] for y in range(y_min, y_max+1)]
             else:
-                for x_y in list_coordinate_for_change:
-                    y, x = x_y
-                    grid.update_matrix({grid[x, y] + 1: [[x, y]]})
-        elif rule_key == "turn off":
-            if part == 1:
-                grid.update_matrix({0: list_coordinate_for_change}, revert_x_y=True)
-            else:
-                for x_y in list_coordinate_for_change:
-                    y, x = x_y
-                    cur_bright = grid[x, y]
-                    bright = 0 if cur_bright - 1 < 0 else cur_bright - 1
-                    grid.update_matrix({bright: [[x, y]]})
-        elif rule_key == "toggle":
-            for x_y in list_coordinate_for_change:
-                y, x = x_y
-                if part == 1:
-                    grid.update_matrix({1: [[x, y]]}) if grid[x, y] == 0 else grid.update_matrix({0: [[x, y]]})
-                else:
-                    grid.update_matrix({grid[x, y] + 2: [[x, y]]})
-        if print_all:
-            print(f"Правило {idx+1}: {rule_str}")
-            print(f"Список точек: {list_coordinate_for_change}")
-            print(f"{msg}: {sum([sum([x for x in x_list]) for x_list in grid.list_data])} лампочек")
-            res = grid.print_matrix()
-            with open("out.txt", "w") as file:
-                file.write(res)
-        if all([out_file, idx+1 == len(list_rule), not print_all]):
-            res = grid.print_matrix()
-            with open("out.txt", "w") as file:
-                file.write(res)
-    return sum([sum([x for x in x_list if x > 0]) for x_list in grid.list_data])
+                coordinate_for_change.extend([[x, y_max] for x in range(x_min, x_max+1)])
+        else:
+            coordinate_for_change.extend([[x_max, y] for y in range(y_min, y_max+1)])
+        update_grid(coordinate_for_change, rule_key, part)
+    return sum([sum([x for x in x_list if x > 0]) for x_list in grid.matrix])
 
 
-# print(f"Решение части 1: {get_count_lamp(10,generate_test_data(10, 5),part=2, print_all=True)}")
-print(f"Решение части 1: {get_count_lamp(1000,list_str)}")
-print(f"Решение части 2: {get_count_lamp(1000,list_str, part=2)}")
-#341389 - слишком мало
+print(f"Решение части 1: {get_solve(1000,list_str, part=1)}")
+print(f"Решение части 2: {get_solve(1000,list_str, part=2)}")
--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import time
 from copy import deepcopy
+import threading
 
 
 def execution_time(func):
@@ -7,36 +8,38 @@ def execution_time(func):
         time_start = time.time()
         res = func(*args, **kwargs)
         time_finish = time.time()
-        execution_time = time_finish - time_start
-        print("Script execution time: %s sec" % execution_time)
+        print(f"Script execution time: {time_finish - time_start}.s sec")
         return res
 
     return wrapped
 
 
-class Matrix():
+class Matrix:
     def __init__(self, list_data: list):
         assert (len([i for i in list_data if len(i) == len(list_data[0])])
                 == len(list_data)), "Это не матрица!Не все списки равны по длине!"
-        self.list_data = list_data
-        self.x_max, self.y_max = self.get_rank_matrix(self.list_data)
+        self.matrix = list_data
+        self.x_max, self.y_max = self.get_rank_matrix(self.matrix)
 
     @staticmethod
     def get_rank_matrix(matrix: list):
         return len(matrix[0]), len(matrix)
 
-    def update_matrix(self, dict_elements: dict, revert_x_y = False):
-        for elem, list_x_y in dict_elements.items():
+    def update_matrix_element(self, elem, x_y):
+        x, y = x_y
+        self.matrix[y][x] = elem
+
+    def update_matrix_list_data(self, data_for_change: dict):
+        for elem, list_x_y in data_for_change.items():
             for x_y in list_x_y:
-                x, y = x_y if not revert_x_y else x_y[::-1]
-                self.list_data[y][x] = elem
+                self.update_matrix_element(elem, x_y)
 
     def __getitem__(self, x_y: tuple):
-        return self.list_data[x_y[1]][x_y[0]]
+        return self.matrix[x_y[1]][x_y[0]]
 
     def find(self, elem: str):
         list_index = []
-        for y, list_y in enumerate(self.list_data):
+        for y, list_y in enumerate(self.matrix):
             for x, elem_ in enumerate(list_y):
                 if elem_ == elem:
                     list_index.append((x, y))
@@ -44,23 +47,24 @@ class Matrix():
 
     def print_matrix(self, dict_elements: dict = None):
         if not dict_elements:
-            x_max, y_max, list_matrix = self.x_max, self.y_max, self.list_data
+            x_max, y_max, list_matrix = self.x_max, self.y_max, self.matrix
         else:
             new_matrix = self.get_matrix_with_change_elements(dict_elements)
             x_max, y_max = self.get_rank_matrix(new_matrix)
             list_matrix = new_matrix
         print(f"""Матрица размером {x_max}x{y_max}""")
         print(f"""Расположение осей""")
-        print(f"""-- -- -- Y\n¦\n¦\n¦\nX""")
+        print(f"""-- -- -- X\n¦\n¦\n¦\nY""")
         res_matrix = "\n".join([" ".join([str(j) for j in i]) for i in list_matrix])
         print(res_matrix)
-        return res_matrix
 
     def get_matrix_with_change_elements(self, dict_elements: dict):
-        new_matrix = deepcopy(self.list_data)
+        new_matrix = deepcopy(self.matrix)
         # изменяем элементы
         for elem, list_coordinate in dict_elements.items():
             for coordinate in list_coordinate:
                 x, y = coordinate
                 new_matrix[y][x] = elem
         return new_matrix
+
+
