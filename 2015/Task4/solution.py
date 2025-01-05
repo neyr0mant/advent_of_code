b6f2193c603e0986a82d87eb38537d0530242466
Neyromant <Neyromant777@gmail.com>
2025-01-05 13:03:05 +0300

    ffdsf

--- 2015/Task4/solution.py
+++ 2015/Task4/solution.py
@@ -6,8 +6,7 @@ def get_min_count(data_str, count_zero):
     while True:
         md5_hash = hashlib.md5()
         md5_hash.update((data_str + str(i)).encode('utf-8'))
-        hex_digest = md5_hash.hexdigest()
-        if hex_digest.startswith(count_zero * "0"):
+        if md5_hash.hexdigest().startswith(count_zero * "0"):
             return i
         i += 1
 
