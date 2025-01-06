3f4d6066400d8a3089619cc72592e3ce94c3951d
Neyromant <Neyromant777@gmail.com>
2025-01-06 16:35:53 +0300

    ffdsf

--- 2015/Task8/solution.py
+++ 2015/Task8/solution.py
@@ -2,11 +2,9 @@ list_str = [i.strip() for i in open("input.txt", 'r')]
 import re
 
 def get_solve(list_str, part =1):
-    all_count, hex_count, repr_count = 0, 0, 0
-    for str_ in list_str:
-        all_count += len(str_)
-        hex_count += len(str_[1:-1].encode().decode('unicode-escape'))
-        repr_count += len(re.sub('"', r'\"', repr(str_)))
+    all_count = sum(len(str_) for str_ in list_str)
+    hex_count = sum(len(str_[1:-1].encode().decode('unicode-escape')) for str_ in list_str)
+    repr_count = sum(len(re.sub('"', r'\"', repr(str_))) for str_ in list_str)
     return all_count - hex_count if part == 1 else repr_count - all_count
 
 print(f"Решение части 1: {get_solve(list_str)}")
