d1d90f7e7d1839aaacee38b4ca35065539cb27ef
Neyromant <Neyromant777@gmail.com>
2025-01-06 19:17:05 +0300

    ffdsf

---
+++ 2022/Task1/solution.py
@@ -0,0 +1,14 @@
+dict_data, count_elf = {}, 0
+for str_ in open("input.txt"):
+    str_ = str_.strip()
+    if str_.isdigit():
+        old_data = dict_data.get(count_elf, 0)
+        dict_data[count_elf] = old_data + int(str_)
+    else:
+        count_elf += 1
+print(f"Решение части 1: {max(list(dict_data.values()))}")
+list_values = [i for i in dict_data.values()]
+list_values.sort(reverse=True)
+print(f"Решение части 2: {list_values[0] + list_values[1] + list_values[2]}")
+
+
