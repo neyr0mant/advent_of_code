d32b2c3d9ff99bd4b56f720de7dfbc4ab706bea3
Neyromant <Neyromant777@gmail.com>
2025-01-07 15:56:07 +0300

    ffdsf

--- 2018/Task1/solution.py
+++ 2018/Task1/solution.py
@@ -1,11 +1,13 @@
 list_freq_str = [i.strip() for i in open('input.txt')]
+
+
 from functions import execution_time
 @execution_time
-def get_solve(list_data,part=1):
+def get_solve(list_data, part = 1):
     if part == 1:
         return eval(''.join([i.strip() for i in open('input.txt')]))
     else:
-        list_freq, double_freq = [0], 0
+        set_freq, last_freq,  double_freq = {0}, 0, 0
         list_rule = []
         for rule in list_data:
             operator = [i for i in ["+", "-"] if i in rule][0]
@@ -13,12 +15,12 @@ def get_solve(list_data,part=1):
             list_rule.append(rule_int)
         while True:
             for rule in list_rule:
-                last_freq = list_freq[-1]
                 new_freq = last_freq + rule
-                if new_freq in list_freq:
+                if new_freq in set_freq:
                     double_freq = new_freq
                     break
-                list_freq.append(new_freq)
+                set_freq.add(new_freq)
+                last_freq = new_freq
             if double_freq:
                 break
         return double_freq
