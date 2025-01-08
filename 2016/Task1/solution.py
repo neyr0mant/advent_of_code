cd48a747f851aadd0be7ace845f6a3fa2c23a9cc
Neyromant <Neyromant777@gmail.com>
2025-01-08 21:01:08 +0300

    ffdsf

--- 2016/Task1/solution.py
+++ 2016/Task1/solution.py
@@ -1,6 +1,6 @@
 list_move = [i.replace(" ", "").split(",") for i in open("input.txt")][0]
 def get_answer(list_move_, part=1):
-    x_santa, y_santa, direction, set_santa, double_place = 0, 0, (0, 1), set(), None
+    x_santa, y_santa, direction, set_santa = 0, 0, (0, 1), set(),
     for idx, data in enumerate(list_move_):
         rule = [i for i in ["L", "R"] if i in data][0]
         direction = (-direction[1], direction[0]) if rule == "L" \
@@ -18,12 +18,9 @@ def get_answer(list_move_, part=1):
             for step in list_step:
                 if step in set_santa:
                     x_santa, y_santa = step
-                    double_place = step
                     break
                 else:
                     set_santa.add(step)
-                if double_place:
-                    break
     return abs(x_santa) + abs(y_santa)
 
 print(f"Решение части 1: {get_answer(list_move)}")
