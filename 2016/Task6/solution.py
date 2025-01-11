f1e1ede956a25c12eb994df6c7fd6ae7a16c0db4
Neyromant <Neyromant777@gmail.com>
2025-01-11 15:26:48 +0300

    ffdsf

--- 2016/Task6/solution.py
+++ 2016/Task6/solution.py
@@ -1,12 +1,9 @@
 list_str = [i.strip() for i in open("input.txt")]
 
 def get_solve(list_str, part =1):
-    len_list_str = len(list_str[0])
-    list_data = []
-    for idx in range(len_list_str):
-        list_data.append([j[idx] for j in list_str])
+    list_data = [[j[idx] for j in list_str] for idx in range(len(list_str[0]))]
     list_out = [sorted(i, key=lambda x: i.count(x))[::-1] if part == 1 else sorted(i, key=lambda x: i.count(x))
-                    for i in list_data]
+                for i in list_data]
     return "".join([i[0] for i in list_out])
 
 print(f"Решение части 1: {get_solve(list_str)}")
