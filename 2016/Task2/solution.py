499abbd2fcb538704dde5c60127e1e534825b0d4
Neyromant <Neyromant777@gmail.com>
2025-01-12 16:44:26 +0300

    ffdsf

--- 2016/Task2/solution.py
+++ 2016/Task2/solution.py
@@ -4,15 +4,10 @@ from functions import execution_time
 @execution_time
 def get_solve(list_rule, part=1):
     rule_move = {"R": (1, 0), "L": (-1, 0), "U": (0, 1), "D": (0, -1)}
-    rule_phone_p1 = {1: (0,2), 2: (1, 2), 3: (2, 2),
-                     4: (0,1), 5: (1, 1), 6: (2, 1),
-                     7: (0,0), 8: (1, 0), 9: (2, 0)}
+    rule_phone_p1 = {1: (0,2), 2: (1, 2), 3: (2, 2), 4: (0,1), 5: (1, 1), 6: (2, 1), 7: (0,0), 8: (1, 0), 9: (2, 0)}
     rule_phone_invert_p1 = {val: key for key, val in rule_phone_p1.items()}
-    rule_phone_p2 = {1: (2,4),
-                     2:  (1,3), 3:  (2,3), 4:  (3,3),
-                     5: (0,2), 6:  (1,2), 7:  (2,2), 8:  (3, 2), 9: (4,2),
-                     "A": (1,1),"B":(2,1), "C":(3,1),
-                     "D": (2,0)}
+    rule_phone_p2 = {1: (2, 4), 2: (1, 3), 3: (2, 3), 4: (3, 3), 5:(0, 2), 6: (1, 2), 7: (2, 2),
+                     8: (3, 2), 9: (4, 2), "A": (1, 1), "B": (2, 1), "C": (3, 1), "D": (2, 0)}
     rule_phone_invert_p2 = {val: key for key, val in rule_phone_p2.items()}
     rule_phone, rule_phone_invert = (rule_phone_p1, rule_phone_invert_p1) if \
         (part == 1) else (rule_phone_p2, rule_phone_invert_p2)
