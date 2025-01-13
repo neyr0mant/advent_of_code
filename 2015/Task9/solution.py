74fd6df2c47ffe7b0b651e3b092cf1e65d173ad3
Neyromant <Neyromant777@gmail.com>
2025-01-13 22:04:40 +0300

    ffdsf

--- 2015/Task9/solution.py
+++ 2015/Task9/solution.py
@@ -1,29 +1,30 @@
 data_str = [[j.strip() for j in i.split("=")] for i in open("input.txt")]
 import itertools
 dict_data = {}
+from functions import execution_time
 
 for i in data_str:
     t1t2 = i[0].split()
     town_1, town_2 = t1t2[0], t1t2[2]
     dict_data.update({(town_1,town_2): int(i[1])})
-
+@execution_time
 def get_solve(data_in, part=1):
     list_keys = list(data_in.keys())
     list_distance = []
-    towns_l, towns_r = [i[0] for i in list_keys],[i[1] for i in list_keys]
-    towns = set(towns_l+ towns_r)
+    town_start, town_finish = [i[0] for i in list_keys],[i[1] for i in list_keys]
+    towns = set(town_start + town_finish)
     list_route = list(itertools.permutations(towns,len(towns)))
     for route in list_route:
         rout_distance = 0
-        for idx, town in enumerate(route):
+        for idx, town1 in enumerate(route):
             if idx + 1 == len(route):
                 break
             else:
                 town2 = route[idx+1]
-                distance = data_in.get((town, town2)) if data_in.get((town, town2)) else data_in.get((town2, town))
+                distance = res if (res := data_in.get((town1, town2))) else data_in.get((town2, town1))
                 rout_distance += distance
         list_distance.append(rout_distance)
-    return min(list_distance) if part ==1 else max(list_distance)
+    return min(list_distance) if part == 1 else max(list_distance)
 
 
 print(f"Решение части 1: {get_solve(dict_data)}")
