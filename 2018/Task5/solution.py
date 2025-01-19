741b0b99ccceb74cc707e752b53aa24601f00551
Neyromant <Neyromant777@gmail.com>
2025-01-19 04:53:17 +0300

    ffdsf

--- 2018/Task5/solution.py
+++ 2018/Task5/solution.py
@@ -2,7 +2,7 @@ data = [i.strip() for i in open("input.txt")][0]
 from functions import execution_time, alphabet
 
 def solve_p1(str_in):
-    letter_out = []
+    letter_out = ""
     for letter in str_in:
         if letter_out:
             conditions = [letter.lower() == letter_out[-1].lower(),
@@ -16,11 +16,8 @@ def solve_p1(str_in):
 
 @execution_time
 def get_solve(str_in, part =1):
-    if part ==1:
-        solve = solve_p1(str_in)
-    else:
-        solve = min([solve_p1(str_in.replace(i, "").replace(i.upper(), "")) for i in alphabet["lower"]])
-    return solve
+    return solve_p1(str_in) if part == 1 else min([solve_p1(str_in.replace(i, "").replace(i.upper(), ""))
+                                                  for i in alphabet["lower"]])
 
 print(f"Решение части 1: {get_solve(data)}")
 print(f"Решение части 2: {get_solve(data, part=2)}")
