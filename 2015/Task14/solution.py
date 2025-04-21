758599980222a53a644a64f13fa2687acbfa1075
Neyromant <Neyromant777@gmail.com>
2025-04-21 13:10:14 +0300

    ffdsf

--- 2015/Task14/solution.py
+++ 2015/Task14/solution.py
@@ -2,10 +2,8 @@ list_str = [i.strip() for i in open("input.txt")]
 dict_data = {}
 for i in list_str:
     list_data = i.split()
-    name = list_data[0]
-    speed_fly = int(list_data[3])
-    fly_sec = int(list_data[6])
-    rest_sec = int(list_data[13])
+    name, speed_fly, fly_sec, rest_sec = (list_data[0], int(list_data[3]),
+                                          int(list_data[6]),int(list_data[13]))
     dict_data.update({name: [speed_fly,fly_sec,rest_sec]})
 def calculate_distance(name, count_sec):
     list_data = dict_data[name]
@@ -22,7 +20,7 @@ def get_solve(part =1):
             dict_distance = {name: calculate_distance(name, time_) for name in dict_data}
             max_val = max(list(dict_distance.values()))
             winner = [i for i in dict_distance if dict_distance[i] == max_val][0]
-            point_dict[winner] +=1
+            point_dict[winner] += 1
         return max(point_dict.values())
 
 
