ee349d3636e5e63d6250aad9b5f19ccf8d15bcde
Neyromant <Neyromant777@gmail.com>
2025-04-23 12:34:44 +0300

    ffdsf

--- 2015/Task17/solution.py
+++ 2015/Task17/solution.py
@@ -4,9 +4,9 @@ def get_solve(part = 1):
     res = 0
     for i in range(1, len(list_data)+1):
         data = [i for i in list(itertools.combinations(list_data, i)) if sum(i) == 150]
-        res += len(data)
         if part != 1 and data:
             return len(data)
+        res += len(data)
     return res
 
 print(f"Решение части 1: {get_solve()}")
