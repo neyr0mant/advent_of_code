3a89ecc89033bf40530641f5e56d1a9b31644bef
Neyromant <Neyromant777@gmail.com>
2025-11-30 00:42:46 +0300

    ffdsf

--- 2024/Task3/solution.py
+++ 2024/Task3/solution.py
@@ -3,32 +3,28 @@ list_data = [i for i in open("input.txt")]
 data = "".join(list_data)
 test_data = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
-def get_multiplication(str_in, part=1):
-    list_data = str_in.split("mul")
-    summ = 0
-    dont = False
-    for i in list_data:
-        list_i =[i.split(")") for i in i.split("(")]
-        if len(list_i) < 2:
+
+def get_solve(s, part=1):
+    total = 0
+    enabled = True
+    for part_str in s.split("mul"):
+        if len(part_str) < 3:
             continue
-        if part ==2:
-            if dont:
-                if "do()" in i:
-                    dont = False
-                continue
-        if list_i[0] == [""]:
-            a_b_list = [int(i) for i in list_i[1][0].split(",") if i.isdigit()]
-            if len(a_b_list) == 2:
-                summ += a_b_list[0]*a_b_list[1]
+        if part_str[0] == "(" and ")" in part_str:
+            args = part_str[1: part_str.index(")")].split(",")
+            if len(args) == 2 and args[0].isdigit() and args[1].isdigit():
+                a, b = int(args[0]), int(args[1])
+                if part == 1 or enabled:
+                    total += a * b
         if part == 2:
-            if "don't()" in i:
-                dont = True
-            if dont:
-                if "do()" in i:
-                    dont = False
-    return summ
-print(f"Решение части 1:{get_multiplication(data, part=1)}")
-print(f"Решение части 2: {get_multiplication(data, part=2)}")
+            if "don't()" in part_str:
+                enabled = False
+            if "do()" in part_str:
+                enabled = True
+
+    return total
+print(f"Решение части 1:{get_solve(data, part=1)}")
+print(f"Решение части 2: {get_solve(data, part=2)}")
 
 
 
