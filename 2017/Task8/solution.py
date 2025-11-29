fe6e0fb93dc1aefd297b0a57eb514a41e84a5b7e
Neyromant <Neyromant777@gmail.com>
2025-11-29 14:07:36 +0300

    ffdsf

--- 2017/Task8/solution.py
+++ 2017/Task8/solution.py
@@ -7,13 +7,11 @@ def get_solve(list_data_, part =1):
             globals()[arg1], dict_data[arg1] = 0, 0
         if arg2 not in globals():
             globals()[arg2], dict_data[arg2] = 0, 0
-        rule = " ".join(i).replace("inc", "+").replace("dec", "-") + f" else {arg1}"
-        new_arg1 = eval(rule)
-        globals()[arg1] = new_arg1
-        dict_data[arg1] = new_arg1
+        new_arg1 = eval(" ".join(i).replace("inc", "+").replace("dec", "-") + f" else {arg1}")
+        globals()[arg1], dict_data[arg1] = new_arg1, new_arg1
         max_ = new_arg1 if new_arg1 > max_ else max_
     for i in dict_data:
-        globals()[i] =0
+        globals()[i] = 0
     return max(dict_data.values()) if part == 1 else max_
 
 print(f"Решение части 1: {get_solve(list_data)}")
