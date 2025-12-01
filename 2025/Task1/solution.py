3fc98c305ff5f48042b0ecce1407359c8a72ff11
Neyromant <Neyromant777@gmail.com>
2025-12-01 17:17:51 +0300

    ffdsf

--- 2025/Task1/solution.py
+++ 2025/Task1/solution.py
@@ -2,14 +2,15 @@ list_data = [i.strip().replace("L", "-").replace("R", "+") for i in open("input.
 def get_solve(list_data_, part=1):
     pos, count_zero = 50, 0
     for cmd in list_data_:
-        delta = eval(cmd)
-        new_pos = (pos + delta) % 100
+        steps = eval(cmd)
+        new_pos = (pos + steps) % 100
         if part == 1:
             if new_pos == 0:
                 count_zero += 1
         else:
-            step = 1 if delta > 0 else -1
-            for _ in range(abs(delta)):
+            step = 1 if steps > 0 else -1
+            count_zero += abs(steps) // 100 # полное количество кругов
+            for _ in range(abs(abs(steps) % 100)): # допроверяем остаток
                 pos = (pos + step) % 100
                 if pos == 0:
                     count_zero += 1
