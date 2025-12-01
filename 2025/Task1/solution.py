93b3062fde58a8610e89e2662c6ff8d94851c514
Neyromant <Neyromant777@gmail.com>
2025-12-01 11:41:30 +0300

    ffdsf

---
+++ 2025/Task1/solution.py
@@ -0,0 +1,20 @@
+list_data = [i.strip().replace("L", "-").replace("R", "+") for i in open("input.txt")]
+def get_solve(list_data_, part=1):
+    pos, count_zero = 50, 0
+    for cmd in list_data_:
+        delta = eval(cmd)
+        new_pos = (pos + delta) % 100
+        if part == 1:
+            if new_pos == 0:
+                count_zero += 1
+        else:
+            step = 1 if delta > 0 else -1
+            for _ in range(abs(delta)):
+                pos = (pos + step) % 100
+                if pos == 0:
+                    count_zero += 1
+        pos = new_pos
+    return count_zero
+
+print(f"Решение части 1: {get_solve(list_data)}")
+print(f"Решение части 2: {get_solve(list_data, part=2)}")
