eef09640ff2b3b459c8b15b27b3924540f496c19
Neyromant <Neyromant777@gmail.com>
2025-12-01 19:46:33 +0300

    ffdsf

--- 2025/Task1/solution.py
+++ 2025/Task1/solution.py
@@ -12,10 +12,11 @@ def get_solve(list_data_, part=1):
             count_circle = abs(steps) // 100
             count_zero += count_circle
             rem_steps = abs(steps) - count_circle * 100
-            if steps > 0 and rem_steps + pos > 99:
-                count_zero += 1
-            elif steps < 0 and pos - rem_steps <= 0 and pos != 0:
-                count_zero += 1
+            if pos != 0:
+                if steps > 0 and rem_steps + pos > 99:
+                    count_zero += 1
+                elif steps < 0 and pos - rem_steps <= 0:
+                    count_zero += 1
         pos = new_pos
     return count_zero
 
