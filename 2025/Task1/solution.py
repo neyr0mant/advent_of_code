def2e3e56e31e6b9979f9c4b16b386301696a32c
Neyromant <Neyromant777@gmail.com>
2025-12-01 17:25:27 +0300

    ffdsf

--- 2025/Task1/solution.py
+++ 2025/Task1/solution.py
@@ -10,7 +10,7 @@ def get_solve(list_data_, part=1):
         else:
             step = 1 if steps > 0 else -1
             count_zero += abs(steps) // 100 # полное количество кругов
-            for _ in range(abs(abs(steps) % 100)): # допроверяем остаток
+            for _ in range(abs(steps) % 100): # допровряем остаток
                 pos = (pos + step) % 100
                 if pos == 0:
                     count_zero += 1
