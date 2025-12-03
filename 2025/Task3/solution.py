a7be6ffa7e3dbdd3dccd04dff42f7a707d2ffd48
Neyromant <Neyromant777@gmail.com>
2025-12-03 12:18:34 +0300

    ffdsf

--- 2025/Task3/solution.py
+++ 2025/Task3/solution.py
@@ -1,21 +1,18 @@
 from functions import *
 list_data = [i.strip() for i in open("input.txt")]
-
-def sort_str(str_, n):
-    len_num = len(str_)
-    delete, acc = len_num - n, []
-    for digit in str_:
-        while acc and delete > 0 and acc[-1] < digit:
-            acc.pop()
-            delete -= 1
-        acc.append(digit)
-    return acc
 @execution_time
 def get_solve(list_data_, part=1):
     n = 2 if part == 1 else 12
     summ_all = 0
     for num in list_data_:
-        summ_all += int("".join(sort_str(num, n)[:n]))
+        len_num = len(num)
+        delete, acc = len_num - n, []
+        for digit in num:
+            while acc and delete > 0 and acc[-1] < digit:
+                acc.pop()
+                delete -= 1
+            acc.append(digit)
+        summ_all += int("".join(acc[:n]))
     return summ_all
 
 print(f"Решение части 1: {get_solve(list_data)}")
