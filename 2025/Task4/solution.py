3af1815e48bff467f712c77b55d2a8331a1c326f
Neyromant <Neyromant777@gmail.com>
2025-12-04 11:52:44 +0300

    ffdsf

--- 2025/Task4/solution.py
+++ 2025/Task4/solution.py
@@ -1,42 +1,37 @@
-#https://adventofcode.com/2024/day/4
-list_data = [i.strip("\n") for i in open("input.txt")]
-
-DATA_V1 = [[[1, 0], [2, 0], [3, 0]],[[1, 1], [2, 2], [3, 3]],[[0, 1], [0, 2], [0, 3]],[[-1, 1], [-2, 2], [-3, 3]],
-           [[-1, 0], [-2, 0], [-3, 0]],[[-1, -1], [-2, -2], [-3, -3]],[[0, -1], [0, -2], [0, -3]],
-           [[1, -1], [2, -2], [3, -3]],]
-DATA_V2 = [[[-1, -1], [1, -1], [1, 1], [-1, 1]],[[1, -1], [1, 1], [-1, -1], [-1, 1]],
-           [[1, 1], [-1, 1], [-1, -1], [1, -1]],[[-1, 1], [-1, -1], [1, 1], [1, -1]]]
-
-def search(x, y, list_word, matrix, word_find):
-    count = 0
-    for data in matrix:
-        len_search = 0
-        for index, delta in enumerate(data):
-            x_delta, y_delta = delta
-            x_, y_ = x + x_delta, y + y_delta
-            if x_ < 0 or y_ < 0:
-                break
-            try:
-                if list_word[y_][x_] != word_find[index]:
-                    break
-            except IndexError:
-                break
-            len_search += 1
-            if len_search == len(word_find):
-                count += 1
-    return count
-
-def get_count_word(list_word, part=1):
-    matrix, word_find, letter_find = (DATA_V1,'MAS', "X") if part == 1 else (DATA_V2, 'MMSS', "A")
+from functions import *
+list_data = [[j for j in i.strip()] for i in open("input.txt")]
+grid = Matrix(list_data)
+from functions import *
+def get_solve_p1(grid_):
+    count_all, vectors = 0, [[0,-1],[-1,-1],[-1, 0],[-1, 1], [0, 1], [1,1], [1, 0], [1, -1]]
+    list_cor_possible = []
+    for y in range(grid_.y_max):
+        for x in range(grid_.x_max):
+            if grid[x, y] == "@":
+                count_neighbours = 0
+                for vector in vectors:
+                    x_n, y_n = x+vector[0], y+vector[1]
+                    if 0 <= x_n <= grid_.x_max-1 and 0 <= y_n <= grid_.y_max-1:
+                        neighbours = grid_[x_n, y_n]
+                        if neighbours == "@":
+                            count_neighbours += 1
+                if count_neighbours < 4:
+                    count_all += 1
+                    list_cor_possible.append([x, y])
+    return count_all, list_cor_possible
+@execution_time
+def get_solve_p2(grid_):
     count_all = 0
-    for y, line in enumerate(list_word):
-        for x, letter in enumerate(line):
-            if list_word[y][x] == letter_find:
-                count_all += search(x, y, list_word, matrix, word_find)
+    count_possible, list_cor_possible = get_solve_p1(grid_)
+    while count_possible:
+        count_possible, list_cor_possible = get_solve_p1(grid_)
+        for x,y in list_cor_possible:
+            grid_[x, y] = "."
+        count_all += count_possible
     return count_all
 
-print(f"Решение части 1: {get_count_word(list_data, part=1)}")
-print(f"Решение части 2: {get_count_word(list_data, part=2)}")
+print(f"Решение части 1: {get_solve_p1(grid)[0]}")
+print(f"Решение части 2: {get_solve_p2(grid)}")
 
 
 
