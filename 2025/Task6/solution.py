8f23699f535ab8ebf84617f7d9a73d1e3e28f971
Neyromant <Neyromant777@gmail.com>
2025-12-06 19:02:10 +0300

    ffdsf

--- 2025/Task6/solution.py
+++ 2025/Task6/solution.py
@@ -2,20 +2,20 @@
 from functions import *
 list_data = [[j for j in i if j != "\n"] for i in open("input.txt")]
 matrix = Matrix(list_data)
+operators = ["*", "+"]
+data_index_operator = {i: [] for i in operators}
+operators_list = matrix.matrix[-1]
+start_index = min([operators_list.index(i) for i in operators])
+while start_index < matrix.x_max:
+    operator = operators_list[start_index]
+    next_list = [operators_list[start_index + 1:].index(i) for i in operators if i in operators_list[start_index + 1:]]
+    end_index = min(next_list) + start_index if next_list else matrix.x_max
+    data_index_operator[operator].append([start_index, end_index])
+    start_index = end_index + 1
+num_data = matrix.matrix[:-1]
 @execution_time
-def get_solve(part =1):
+def get_solve(part = 1):
     sum_all = 0
-    operators = ["*", "+"]
-    data_index_operator = {i: [] for i in operators}
-    operators_list = matrix.matrix[-1]
-    start_index = min([operators_list.index(i) for i in operators])
-    while start_index < matrix.x_max:
-        operator = operators_list[start_index]
-        next_list = [operators_list[start_index+1:].index(i) for i in operators if i in operators_list[start_index+1:]]
-        end_index = min(next_list) + start_index if next_list else matrix.x_max
-        data_index_operator[operator].append([start_index, end_index])
-        start_index = end_index + 1
-    num_data = matrix.matrix[:-1]
     for operator, list_cor in data_index_operator.items():
         for cor in list_cor:
             start, end = cor
