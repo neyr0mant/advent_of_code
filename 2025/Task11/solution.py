b187fb7b312d944b74d329f9d765539ff9d6dbcf
Neyromant <Neyromant777@gmail.com>
2025-12-11 10:27:35 +0300

    ffdsf

--- 2025/Task11/solution.py
+++ 2025/Task11/solution.py
@@ -1,25 +1,22 @@
 #https://adventofcode.com/2024/day/11
-from functions import  execution_time
-def get_next_list_stones(num):
-    if num == 0:
-        return [1]
-    str_num = str(num)
-    len_num = len(str_num)
-    return [int(str_num[:len_num // 2]), int(str_num[len_num // 2:])] if len_num % 2 == 0 else [num * 2024]
-
-@execution_time
-def get_count_stones(list_stones, count_iter):
-    start_stones = {stone: 1 for stone in list_stones}
-    for _ in range(count_iter):
-        next_stones = {}
-        for сur_stone, count in start_stones.items():
-            new_stones = get_next_list_stones(сur_stone)
-            for stone in new_stones:
-                if stone in next_stones:
-                    next_stones[stone] += count
-                else:
-                    next_stones[stone] = count
-        start_stones = next_stones
-    return sum(start_stones.values())
-print(f"Решение части 1: {get_count_stones([2, 54, 992917, 5270417, 2514, 28561, 0, 990], 25)}")
-print(f"Решение части 2: {get_count_stones([2, 54, 992917, 5270417, 2514, 28561, 0, 990], 75)}")
+dict_data = {i.strip().split(":")[0]: i.strip().split(":")[1].split() for i in open("input.txt")}
+def get_solve(dict_data_, part = 1):
+    solve = 0
+    if part == 1:
+        start, end = 'you', 'out'
+        list_start = [(start, [start])]
+        all_paths = []
+        while list_start:
+            cur, path = list_start.pop()
+            if cur == end:
+                all_paths.append(path)
+                continue
+            if cur in dict_data_:
+                for neighbor in dict_data_[cur]:
+                    if neighbor not in path:
+                        new_path = path + [neighbor]
+                        list_start.append((neighbor, new_path))
+        solve = len(all_paths)
+    return solve
+print(f"Решение части 1: {get_solve(dict_data)}")
+# print(f"Решение части 2: {get_solve()}")
